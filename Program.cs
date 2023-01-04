a9b80784b48d7c241b08fafb7f314dd43cb34127
Ignat Pocevski <ignat.pocevski@gmail.com>
2023-01-04 16:59:46 +0100

    Added AzureContext

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using web.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DoktorEContext");
+var connectionString = builder.Configuration.GetConnectionString("DoktorEContextAzure");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
