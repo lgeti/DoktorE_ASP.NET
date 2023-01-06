dc6b241cec34c8a4fe2ba866e9303d17799f553a
Ignat Pocevski <ignat.pocevski@gmail.com>
2023-01-06 17:59:17 +0100

    ApiKeyAuth implemented on PatientsApiController.

--- Controllers/Api/PatientsApiController.cs
+++ Controllers/Api/PatientsApiController.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using web.Data;
 using web.Models;
+using web.Filters;
 
 namespace web.Controllers_Api
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ApiKeyAuth]
     public class PatientsApiController : ControllerBase
     {
         private readonly DoktorEContext _context;
